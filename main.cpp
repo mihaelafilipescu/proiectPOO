d36e69f8867356adf92115de8f0b8d458b3b232d
mihaelafilipescu <mfilipescu22@gmail.com>
2025-03-17 13:17:51 +0200

    cerinte tema 1 4 clase -> constructori, cc

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
 
 class Animal {
     std::string Name;
@@ -8,7 +9,8 @@ class Animal {
     int LevelUnlock;
     int FeedTime;
     std::string ResultedGood;
-    std::string Fed;
+    int Fed;
+    std::chrono::steady_clock::time_point lastFedTime; // Momentul ultimei hrăniri
 
 public:
     const std::string& getName() const { return Name; };
@@ -16,8 +18,9 @@ public:
     int getFeedTime() const { return FeedTime; };
     int getLevelUnlock() const { return LevelUnlock; };
     const std::string& getResultedGood() const { return ResultedGood; };
-    const std::string& getFed() const { return Fed; };
-    Animal(const std::string& x) : Fed{x} {};
+    int getFed() const { return Fed; };
+    int setFed(int Fed_) { Fed = Fed_; };
+    Animal(const int x) : Fed{x} {};
     Animal(const std::string& Name_, int Cost_, int LevelUnlock_, int FeedTime_, const std::string& ResultedGood_) : Name{Name_}, Cost{Cost_},
         LevelUnlock{LevelUnlock_}, FeedTime{FeedTime_}, ResultedGood{ResultedGood_} {};
     Animal(const Animal& other) : Name{other.Name}, Cost{other.Cost},
@@ -25,17 +28,44 @@ public:
 
     ~Animal() = default;
 
-
     void feedAnimal(Animal& animal) {
+        int intervalSec = animal.getFeedTime();
         std::string raspuns;
-        std::cout<<"Vrei sa hranesti" << animal.getName() <<"? (da/nu)\n";
+        std::string raspuns2;
+        std::cout<<"Vrei sa hranesti " << animal.getName() <<"? (da/nu)\n";
         std::cin >> raspuns;
-        if (raspuns == "DA" || raspuns == "da" || raspuns == "Da") {
-            Animal animal{"true" };
-            std::cout << animal.getName() <<" a fost hranit!\n";
-        } else {
-            std::cout << animal.getName() << " nu a fost hrănit!\n";
+        if (animal.getFed() == 0 || canBeFed(intervalSec)){
+            if (animal.getName() == "Goat"){
+                std:: cout<< "Cine este adevaratul " << animal.getName() << " ??????????????????????\n";
+                std:: cout<< "Messi sau Ronaldo? \n" ;
+                std:: cin >> raspuns2;
+                if (raspuns2 == "Ronaldo" || raspuns2 == "RONALDO"){
+                    std::cout <<"Raspuns gresit! Nu poti hrani "<< animal.getName() << "\n";
+                    raspuns = "nu";
+                }
+                else{
+                    std:: cout<< "Raspuns corect!\n";
+                }
+            }
+            if (raspuns == "DA" || raspuns == "da" || raspuns == "Da") {
+                //baga operator de afisare;
+                animal.setFed(1);
+                lastFedTime = std::chrono::steady_clock::now();
+                std::cout << animal.getName() <<" a fost hranit!\n";
+            }
+            else {
+                std::cout << animal.getName() << " nu a fost hranit!\n";
+            }
         }
+         else{
+             std:: cout<< animal.getName() << " a fost deja hranit!";
+         }
+    }
+
+    bool canBeFed(int intervalSec) {
+        auto now = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - lastFedTime).count();
+        return elapsed >= intervalSec;
     }
 
 };
@@ -52,6 +82,8 @@ public:
     int getGrowTime() const { return GrowTime; };
     Plant(const std::string& Name_, int Cost_, int LevelUnlock_, int GrowTime_): Name{Name_}, Cost{Cost_},
         LevelUnlock{LevelUnlock_}, GrowTime{GrowTime_}{};
+
+    ~Plant() = default;
 };
 
 class Machine {
@@ -68,6 +100,8 @@ public:
     int getMaintenance() const { return Maintenance; };
     Machine(const std::string& Name_, int Cost_, int LevelUnlock_, int BuildTime_, int Maintenance_) : Name{Name_}, Cost{Cost_},
         LevelUnlock{LevelUnlock_}, BuildTime{BuildTime_}, Maintenance{Maintenance_}{};
+
+    ~Machine() = default;
 };
 
 class Silo {
@@ -80,11 +114,11 @@ class Silo {
 
 
 int main() {
-    Animal Chicken("Chicken", 20, 1, 20, "Egg"),
-           Cow("Cow", 25, 5, 60, "Milk"),
-           Pig("Pig", 35, 10, 90, "Bacon"),
-           Sheep("Sheep", 40, 25, 360, "Wool"),
-           Goat("Goat", 70, 38, 480, "Goat Milk");
+    Animal Chicken("Kitchen", 20, 1, 20, "Aripioare picante"),
+           Cow("Cow", 25, 5, 60, "Brinza"),
+           Pig("Pig", 35, 10, 90, "Sorici"),
+           Sheep("Sheep", 40, 25, 360, "Ciubotele"),
+           Goat("Goat", 70, 38, 480, "Baby Messi");
     Plant Wheat("Wheat", 2, 1, 2),
           Corn("Corn", 5, 3, 5),
           Bean("Soybeans", 10, 5, 20),
@@ -94,6 +128,9 @@ int main() {
             Popcorn("PopcornPot", 100, 5, 30, 100),
             Oven("Oven", 150, 10, 60, 150),
             Grill("Grill", 200, 15, 120, 160);
+
+    Goat.feedAnimal(Goat);
+
 
     return 0;
 }
