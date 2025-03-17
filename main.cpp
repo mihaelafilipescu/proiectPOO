3b8ed2c40dd86a5584f7504d11933fab063b226a
mihaelafilipescu <mfilipescu22@gmail.com>
2025-03-17 17:06:11 +0200

    cerinte tema 1 4 clase -> constructori, cc

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ public:
     int getFed() { return Fed; };
     void setFed( int Fed_) { Fed = Fed_; };
     Animal(const int x) : Fed{x} {};
-    Animal(const std::string& Name_, int Cost_, int LevelUnlock_, int FeedTime_, const std::string& ResultedGood_) : Name{Name_}, Cost{Cost_},
+    Animal( const std::string& Name_, int Cost_, int LevelUnlock_, int FeedTime_, const std::string& ResultedGood_) : Name{Name_}, Cost{Cost_},
         LevelUnlock{LevelUnlock_}, FeedTime{FeedTime_}, ResultedGood{ResultedGood_} {};
     Animal(const Animal& other) : Name{other.Name}, Cost{other.Cost},
         LevelUnlock{other.LevelUnlock}, FeedTime{other.FeedTime}, ResultedGood{other.ResultedGood}{};
