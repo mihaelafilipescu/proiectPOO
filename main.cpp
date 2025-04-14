c946a31ba008579179188ca4a1f686ceab1cab83
mihaelafilipescu <mfilipescu22@gmail.com>
2025-04-14 14:09:43 +0300

    cerinte tema 1 4 clase -> constructori, cc

--- main.cpp
+++ main.cpp
@@ -4,7 +4,38 @@
 #include <string>
 #include <chrono>
 #include <ostream>
+// #include "ext/indicators.hpp"
+// #include "ext/progress_bar.hpp"
+// #include "ext/cursor_control.hpp"
 #include <thread>
+
+// class progressBar {
+// public:
+//     using namespace indicators;
+//
+//     indicators::ProgressBar bar{
+//         indicators::option::BarWidth{50},
+//         indicators::option::Start{" ["},
+//         indicators::option::Fill{"█"},
+//         indicators::option::Lead{"█"},
+//         indicators::option::Remainder{"-"},
+//         indicators::option::End{"]"},
+//         indicators::option::PrefixText{"Training Gaze Network 👀"},
+//         indicators::option::ForegroundColor{indicators::Color::yellow},
+//         indicators::option::ShowElapsedTime{true},
+//         indicators::option::ShowRemainingTime{true},
+//         indicators::option::FontStyles{std::vector<indicators::FontStyle>{indicators::FontStyle::bold}}
+//     };
+//
+//     while (true){
+//         bar.tick();
+//         if (bar.is_completed())
+//             break;
+//         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+//     }
+//
+// };
+
 
 class Animal {
     std::string Name;
@@ -21,11 +52,7 @@ public:
     int getRestultedMoney() const { return resultedMoney; };
     explicit Animal() : Cost(0), FeedTime(0), resultedMoney(0) {
     };
-    // Animal( std::string  Name_, const int Cost_, const int LevelUnlock_, const int FeedTime_, std::string  ResultedGood_) : Name{std::move(
-    //         Name_
-    //     )}, Cost{Cost_},
-    //     LevelUnlock{LevelUnlock_}, FeedTime{FeedTime_}, ResultedGood{std::move(ResultedGood_)} {
-    // };
+
     Animal(const Animal& other) : Name{other.Name}, Cost{other.Cost},
                                   FeedTime{other.FeedTime},
                                   ResultedGood{other.ResultedGood}, resultedMoney{other.resultedMoney} {
@@ -111,17 +138,24 @@ public:
         return *this;
     }
 
+    friend std::ostream& operator<<(std::ostream& os, const Silo& silo) {
+        for (const auto& stored_plants : silo.storedPlants) {
+            os << stored_plants.first << " " << stored_plants.second << " ";
+        }
+        return os;
+    };
+
     // Adaugă plante în Silo
     void storePlants( const std::string& plantName, int quantity) {
         for (size_t i = 0 ; i < storedPlants.size(); ++i) {
             if (storedPlants[i].first == plantName) {
                 storedPlants[i].second += quantity;
-                std::cout << quantity << " " << plantName << " adaugate in silo. \n";
+                // std::cout << quantity << " " << plantName << " adaugate in silo. \n";
                 return;
             }
         }
         storedPlants.emplace_back(plantName, quantity);
-        std::cout << quantity << " " << plantName << " adaugate in silo. \n";
+        // std::cout << quantity << " " << plantName << " adaugate in silo. \n";
     }
     void siloContent() const {
         std::cout<< "In silo ai: \n";
@@ -155,29 +189,30 @@ public:
         return *this;
     }
 
-    Barn & operator=(Barn &&other) noexcept {
-        if (this == &other)
-            return *this;
-        storedItems = std::move(other.storedItems);
-        return *this;
-    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Barn& barn) {
+        for (const auto& stored_items : barn.storedItems) {
+            os << stored_items.first << " " << stored_items.second << "\n";
+        }
+        return os;
+    };
 
     void storeItems( const std::string& itemName, int quantity) {
         for (size_t i = 0 ; i < storedItems.size(); i++) {
             if (storedItems[i].first == itemName) {
                 storedItems[i].second += quantity;
-                std::cout << quantity << " " << itemName << " adaugate in barn. \n";
+                // std::cout << quantity << " " << itemName << " adaugate in barn. \n";
                 return;
             }
         }
         storedItems.emplace_back(itemName, quantity);
-        std::cout << quantity << " " << itemName << " adaugate in barn. \n";
+        // std::cout << quantity << " " << itemName << " adaugate in barn. \n";
     }
 
     void barnContent() const {
         std::cout<< "In barn ai: \n";
         for (size_t i = 0 ; i < storedItems.size(); i++) {
-            std::cout << "- " << storedItems[i].first << " " << storedItems[i].second << "\n";
+            std::cout << "- " << storedItems[i].first << ": " << storedItems[i].second << "\n";
         }
     }
 };
@@ -230,19 +265,27 @@ public:
     }
     ~Farm() = default;
 
+    friend std::ostream& operator<<(std::ostream& os, const Farm& farm) {
+        os << "In ferma ai: \n";
+        os << "- " << farm.money << " bani \n";
+        os << "- " << farm.silo << "\n";
+        os << "- " << farm.barn << "\n";
+    }
+
 
     void harvestPlant(const Plant& plant) {
         int count;
-        std::cout << "Cate " << plant.getName() << " vrei sa plantezi?\n";
+        std::cout << "Cate kilograme de " << plant.getName() << " vrei sa plantezi?\n";
         std::cin >> count;
         if (count >= 1) {
             int growTimeInSeconds = plant.getGrowTime();
             const std::chrono::seconds waitingTime(count * growTimeInSeconds);
-            std:: cout << "Ai plantat " << count << " "  << plant.getName() << " ! Trebuie sa astepti "
+            std:: cout << "Ai plantat " << count << " kg de "  << plant.getName() << "! Trebuie sa astepti "
                             << waitingTime << " secunde ca sa poti sa le culegi!\n";
             std::this_thread::sleep_for(waitingTime);
-            std:: cout << "Felicitari, ai cules " << count << " " << plant.getName() << "!\n";
+            std:: cout << "Felicitari, ai cules " << count << " kg de " << plant.getName() << "!\n";
             silo.storePlants(plant.getName(), count);
+
             money += (plant.getResultedMoney()*count);
             std::cout << "Acum ai " << money << " de bani!\n";
         }
@@ -256,7 +299,7 @@ public:
         if (animal.getName() == "capre"){
             std::string raspuns2;
 
-            std:: cout<< "Cine este adevaratul " << animal.getName() << " ??????????????????????\n";
+            std:: cout<< "Cine este adevaratul " << animal.getName() << "??????????????????????\n";
             std:: cout<< "Messi sau Ronaldo? \n" ;
             std:: cin >> raspuns2;
             if (raspuns2 == "Ronaldo" || raspuns2 == "RONALDO" || raspuns2 == "ronaldo"){
@@ -270,7 +313,7 @@ public:
         if (raspuns == "DA" || raspuns == "da" || raspuns == "Da") {
             const int feedTime = animal.getFeedTime();
             const std::chrono::seconds waitingTime(feedTime);
-            std::cout << "Trebuie sa astepti " << animal.getFeedTime() << " secunde ca " << animal.getName() << " sa fie hranit!\n";
+            std::cout << "Trebuie sa astepti " << animal.getFeedTime() << " secunde ca " << animal.getName() << " sa fie hranite!\n";
             std::this_thread::sleep_for(waitingTime);;
             std::cout << "Felicitari, ai primit " << animal.getResultedGood() << " pentru ca ai hranit " << animal.getName() << "!\n";
             barn.storeItems(animal.getResultedGood(), 1);
@@ -319,18 +362,6 @@ public:
         }
     }
 
-    // void Bakery (const Machine& bakery) {
-    //     int raspuns;
-    //     std :: cout << "Ce iti doresti sa prepari in bakery?\n";
-    //     std :: cout << "- paine (1)\n" << "- mamaliga (2)\n" << "- briose cu zmeura (3)\n" << "- briose cu mure (4)\n";
-    //     switch (raspuns) {
-    //         case 1:
-    //         case 2:
-    //         case 3:
-    //         case 4:
-    //     }
-    // }
-
 };
 
 
@@ -340,11 +371,12 @@ int main() {
     Silo mySilo;
     Barn myBarn;
     Farm myFarm(mySilo, myBarn);
-    Animal Chicken("gaini", 20, 2, "Oua", 2),
-           Cow("vaci", 25,  5, "Lapte", 5),
-           Pig("porci", 35,  7, "Bacon", 12),
-           Sheep("oi", 40,  12, "Lana", 15),
-           Goat("capre", 70,  15, "Branza", 20);
+    // std :: cout<< myFarm;
+    Animal Chicken("gainile", 20, 2, "Oua", 2),
+           Cow("vacile", 25,  5, "Lapte", 5),
+           Pig("porcii", 35,  7, "Bacon", 12),
+           Sheep("oile", 40,  12, "Lana", 15),
+           Goat("caprele", 70,  15, "Branza", 20);
     Plant Wheat("grau", 2, 2, 1),
           Corn("porumb", 5, 5, 3),
           Bean("fasole", 10, 10, 7),
@@ -354,7 +386,7 @@ int main() {
             Popcorn("PopcornPot", 100,  30, 1000),
             Oven("Oven", 150,  60, 1500),
             Grill("Grill", 200,  120, 1600);
-    std :: cout << "Bine ai venit in feram ta!\n" << "Ce doresti sa faci acum?\n";
+    std :: cout << "Bine ai venit in ferma ta!\n" << "Ce doresti sa faci acum?\n";
     std :: cout << "- sa plantez recolta (1) \n" << "- sa hranesc animalele (2) \n" << "- sa construiesc o masinarie (3)\n" << "- sa repar o masinarie (4)\n";
     int ans1, ans2, ans4, ans5;
     std :: string ans3;
@@ -420,7 +452,7 @@ int main() {
                         break;
                 }break;
             case 4:
-                std::cout << "Ce masinarie doresti sa-ti repari?\n";
+                std::cout << "Ce masinarie doresti sa repari?\n";
             std::cout << "- brutarie (1)\n" << "- cuptor (2)\n" << "- masina de popcorn (3)\n" << "- gratar (4)\n";
             std :: cin >> ans5;
             switch (ans5) {
